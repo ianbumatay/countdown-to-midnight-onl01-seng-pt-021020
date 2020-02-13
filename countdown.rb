4099f841d0d9bd2a94b8e57e910ee02ce4f1e9a1
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-13 15:11:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown
-    number = 5
+    number = 10
     while number > 0
       puts "#{number} SECOND(S)!"
       number -= 1
@@ -11,7 +11,7 @@ end
 
 
  def countdown_with_sleep
-   number = 5
+   number = 10
    sleep 3
    while number > 0
      puts "#{number} SECOND(S)!"
